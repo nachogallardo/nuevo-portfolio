15b13795bdbc0d9c7ef59e0aa69b08b79af1c38b
nachogallardo <nacho-gallardo-sanchez@hotmail.com>
2025-10-05 13:44:13 +0200

    ---

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,12 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
         hostname: 'images.credly.com',
         port: '',
         pathname: '/**',
