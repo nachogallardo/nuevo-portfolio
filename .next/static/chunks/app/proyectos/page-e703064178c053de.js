5e01008d686c38b62ba26f902af9ee2df0adbf94
nachogallardo <nacho-gallardo-sanchez@hotmail.com>
2025-10-06 10:34:10 +0200

    Add build folder (.next) to repository

---
+++ .next/static/chunks/app/proyectos/page-e703064178c053de.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[741],{45:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,3063,23)),Promise.resolve().then(n.bind(n,8957))}},e=>{var s=s=>e(e.s=s);e.O(0,[710,774,957,441,684,358],()=>s(45)),_N_E=e.O()}]);
